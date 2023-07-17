ed8fe770f89260917a7d7d7c9d0ab1abc077f0ce
Amit Sharma <amshaupdates@gmail.com>
2023-07-17 10:06:08 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,34 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// reactElement() is an object which gets converted into HTML that browser understands.
-
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [
-        React.createElement(
-            "div",
-            {id: "child"},
-            [React.createElement("h1", {}, "This is Namaste React."), React.createElement("h2", {}, "I'm an h2 tag")]
-        ),
-        React.createElement(
-            "div",
-            {id: "child2"},
-            [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
-        )
-    ]
+const Title = () => (
+  <h1 className="head" tabIndex={5}>
+    Namaste React using JSX
+  </h1>
 );
 
-// To counter this bogus code using react, we use JSX
-// The above code using createElement is the core react
-// to show how react works behind the scene  with props, children etc.
-
-
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+const HeadingComponent = () => {
+  return (
+    <>
+      <div id="container">
+        <Title />
+        <h1 className="heading">Namaste React Functional Component</h1>
+      </div>
+      <div id="container2">
+          <Title />
+          <h1 className="heading">Namaste React Functional Component</h1>
+      </div>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(parent); //object
-
-root.render(parent);
+root.render(<HeadingComponent />);
